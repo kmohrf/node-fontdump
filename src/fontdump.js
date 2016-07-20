54aa872c657b5c59f97284f195a0f19a5d433b12
Konrad Mohrfeldt <mohrfeldt@silicann.com>
2016-07-20 22:52:32 +0200

    improve fontdump module
    * update packages
    * drop custom log module
    * use es-2015 features
    * fix global vars
    * always return promise when calling dump
    * fix woff2 user-agent string

--- src/fontdump.js
+++ src/fontdump.js
@@ -1,18 +1,27 @@
-var css = require("css");
-var request = require("request");
-var rp = require("request-promise");
-var Promise = require("es6-promise").Promise;
-var _ = require("lodash");
-var sprintf = require("sprintf");
-var path = require("path");
-var fs = require("fs");
-var logger = require("./log").get_logger({ module: "fontdump" });
+const crypto = require("crypto");
+const path = require("path");
+const fs = require("fs");
+
+const Promise = require("es6-promise").Promise;
+const request = require("request-promise");
+const logger = require("winston");
+const _ = require("lodash");
+const css = require("css");
+
 
 function hash(data) {
-    return require("crypto").createHash("md5").update(data).digest("hex");
+    return crypto.createHash("md5").update(data).digest("hex");
 }
 
-var FontSource = function(font, source, extension, format) {
+function strip_quotes(string, quotation_style="'") {
+    if(string.indexOf(quotation_style) === 0 && string.lastIndexOf(quotation_style) === string.length - 1) {
+        return string.slice(1, -1);
+    }
+
+    return string;
+}
+
+const FontSource = function(font, source, extension, format) {
     this.font = font;
     this.source = source;
     this.extension = extension;
@@ -21,25 +30,23 @@ var FontSource = function(font, source, extension, format) {
 
 FontSource.prototype = {
     get_filename: function() {
-        return sprintf(
-            "%s_%s_%s_%s.%s",
-            this.font.family.name, this.font.weight, this.font.style,
-            this.font.unicode_range ? hash(this.font.unicode_range) : "default", this.extension
-        ).replace(/\s+/, "").toLowerCase();
+        const range = this.font.unicode_range ? hash(this.font.unicode_range) : "default";
+        return `${this.font.family.name}_${this.font.weight}_${this.font.style}_${range}.${this.extension}`
+            .replace(/\s+/, "").toLowerCase();
     },
     get_web_filename: function() {
-        var filename = this.get_filename();
+        const filename = this.get_filename();
 
         if(this.format !== "svg") {
             return filename;
         }
 
-        var hash_pos = this.source.indexOf("#");
+        const hash_pos = this.source.indexOf("#");
         return filename + (hash_pos !== -1 ? this.source.substr(hash_pos) : "");
     }
 };
 
-var Font = function(family, weight, style, unicode_range) {
+const Font = function(family, weight, style, unicode_range) {
     this.family = family;
     this.weight = weight;
     this.style = style;
@@ -56,7 +63,7 @@ Font.prototype = {
         return _.values(this.sources);
     },
     has_format: function(format) {
-        return this.sources.hasOwnProperty(format);
+        return _.has(this.sources, format);
     },
     get_source: function(format) {
         return this.sources[format]
@@ -71,12 +78,12 @@ Font.FORMATS = {
     "svg": "svg"
 };
 
-var FontFamily = function(name) {
+const FontFamily = function(name) {
     this.name = name;
     this.fonts = {};
 
     this._create_font_id = function(weight, style, unicode_range) {
-        return sprintf("%s_%s_%s", weight, style, hash(unicode_range || ""));
+        return `${weight}_${style}_${hash(unicode_range || "")}`;
     }
 };
 
@@ -85,7 +92,7 @@ FontFamily.prototype = {
         return _.values(this.fonts);
     },
     with_font: function(weight, style, unicode_range) {
-        var id = this._create_font_id(weight, style, unicode_range);
+        const id = this._create_font_id(weight, style, unicode_range);
 
         if(typeof this.fonts[id] === "undefined") {
             this.fonts[id] = new Font(this, weight, style, unicode_range);
@@ -95,13 +102,13 @@ FontFamily.prototype = {
     }
 };
 
-var FontFamilyCollection = function() {
+const FontFamilyCollection = function() {
     this.families = {};
 };
 
 FontFamilyCollection.prototype = {
     sync_fonts: function(synchronizer) {
-        var syncs = this.get_font_sources().map((function(font_source) {
+        const syncs = this.get_font_sources().map((function(font_source) {
             return synchronizer(font_source.source, font_source.get_filename());
         }).bind(this));
 
@@ -129,33 +136,33 @@ FontFamilyCollection.prototype = {
     }
 };
 
-var FontLoader = function(endpoint, target_directory) {
-    var self = this;
+const FontLoader = function(endpoint, target_directory) {
+    const self = this;
 
     this.agents = _.clone(FontLoader.AGENTS);
     this.endpoint = endpoint;
     this.target_directory = target_directory;
 
     this._create_request = function(endpoint, ua, encoding) {
-        request = rp({
+        const req = request({
             url: endpoint,
             encoding: typeof encoding === "undefined" ? "utf8" : null,
             headers: { "User-Agent": ua || FontLoader.AGENTS.woff }
-        }).promise();
-
-        request.then(function() {
-            // on success
-            logger.debug(sprintf("successfully downloaded '%s' with ua '%s'", endpoint, ua));
-        }, function() {
-            // on error
-            logger.error(sprintf("error while downloading '%s' with ua '%s'", endpoint, ua));
         });
 
-        return request;
+        req.then(function() {
+            // on success
+            logger.debug(`successfully downloaded '${endpoint}' with ua '${ua}'`);
+        }, function(err) {
+            // on error
+            logger.error(`error while downloading '${endpoint}' with ua '${ua}'`, err);
+        });
+
+        return req;
     };
 
     this._load_stylesheets = function() {
-        var requests = _.values(this.agents).map(function(ua) {
+        const requests = _.values(this.agents).map(function(ua) {
             return self._create_request(self.endpoint, ua);
         });
 
@@ -166,20 +173,18 @@ var FontLoader = function(endpoint, target_directory) {
         css.parse(stylesheet).stylesheet.rules.forEach(function (rule) {
             if(rule.type !== "font-face") return;
 
-            var declarations = _.indexBy(rule.declarations, "property");
-            var weight = declarations["font-weight"]["value"];
-            var style = declarations["font-style"]["value"];
-            var source_matches = declarations["src"]["value"]
+            const declarations = _.keyBy(rule.declarations, "property");
+            const weight = declarations["font-weight"]["value"];
+            const style = declarations["font-style"]["value"];
+            const source_matches = declarations["src"]["value"]
                 .match(/(url\((http.+)\) format\('(.+)'\)|url\((http.+.eot)\))/);
-            var source = source_matches[2] ? source_matches[2] : source_matches[4];
-            var format = source_matches[2] ? source_matches[3] : "embedded-opentype";
-            var extension = Font.FORMATS[format];
-            var unicode_range = Object.prototype.hasOwnProperty.call(declarations, "unicode-range")
-                ? declarations["unicode-range"]["value"] : null;
-            var family = declarations["font-family"]["value"];
-            family = family.indexOf("'") === 0 ? family.slice(1, -1) : family;
+            const source = source_matches[2] ? source_matches[2] : source_matches[4];
+            const format = source_matches[2] ? source_matches[3] : "embedded-opentype";
+            const extension = Font.FORMATS[format];
+            const unicode_range = _.has(declarations, "unicode-range") ? declarations["unicode-range"]["value"] : null;
+            const family = strip_quotes(declarations["font-family"]["value"]);
 
-            logger.debug(sprintf("adding font %s with weight %s and style %s", family, weight, style));
+            logger.debug(`adding font ${family} with weight ${weight} and style ${style}`);
 
             collection
                 .with_font_family(family)
@@ -190,8 +195,8 @@ var FontLoader = function(endpoint, target_directory) {
 
     this._parse_stylesheets = function(stylesheets) {
         logger.info("parsing stylesheets");
-        return new Promise((function(resolve, reject) {
-            var collection = new FontFamilyCollection();
+        return new Promise((function(resolve) {
+            const collection = new FontFamilyCollection();
             stylesheets.forEach(this._parse_stylesheet.bind(this, collection));
             resolve(collection);
         }).bind(this));
@@ -207,7 +212,7 @@ var FontLoader = function(endpoint, target_directory) {
     };
 
     this._sync_font = function(font_source, filename) {
-        var target_file = path.join(this.target_directory, filename);
+        const target_file = path.join(this.target_directory, filename);
 
         return this
             ._create_request(font_source, null, null)
@@ -222,8 +227,8 @@ var FontLoader = function(endpoint, target_directory) {
                 return new Promise(function(resolve) {
                     resolve(collection)
                 });
-            }, function() {
-                logger.error("could not sync font");
+            }, function(err) {
+                logger.error("could not sync font", err);
             });
     };
 };
@@ -236,7 +241,7 @@ FontLoader.prototype = {
         ;
     },
     disable_format: function(definition_name) {
-        if(this.agents.hasOwnProperty(definition_name)) {
+        if(_.has(this.agents, definition_name)) {
             delete this.agents[definition_name];
         } else {
             throw new Error("no definition with that name or you’ve already disabled it")
@@ -247,22 +252,22 @@ FontLoader.prototype = {
 };
 
 FontLoader.AGENTS = {
-    "woff2": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+    "woff2": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36",
     "woff": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36",
     "ttf": "Mozilla/5.0 (Linux; U; Android 2.2; en-us; DROID2 GLOBAL Build/S273) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
     "svg": "Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10",
     "eot": "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)"
 };
 
-FontFaceRenderer = function(endpoint) {
+const FontFaceRenderer = function(endpoint) {
     this.endpoint = endpoint;
 
     this._create_url = function(font_source) {
-        return sprintf("url('%s')",
-            this.endpoint
-                ? this.endpoint + "/" + font_source.get_web_filename()
-                : font_source.get_web_filename()
-        );
+        const url = this.endpoint
+            ? this.endpoint + "/" + font_source.get_web_filename()
+            : font_source.get_web_filename();
+
+        return `url('${url}')`;
     };
 
     this._add_source_set = function(declaration_set, font) {
@@ -274,7 +279,7 @@ FontFaceRenderer = function(endpoint) {
         }
 
         this._add_declaration(declaration_set, "src", font.get_sources().map((function(source) {
-            return sprintf("%s format('%s')", this._create_url(source), source.format);
+            return `${this._create_url(source)} format('${source.format}')`;
         }).bind(this)).join(", "));
     };
 
@@ -287,7 +292,7 @@ FontFaceRenderer = function(endpoint) {
     };
 
     this._add_rule = function(ruleset, font) {
-        var rule = {
+        const rule = {
             type: "font-face",
             declarations: []
         };
@@ -301,11 +306,10 @@ FontFaceRenderer = function(endpoint) {
             this._add_declaration(rule.declarations, "unicode-range", font.unicode_range);
         }
 
-        logger.debug(sprintf(
-            "adding font-face rule for font-family %s with weight %s and style %s%s",
-            font.family.name, font.weight, font.style, font.unicode_range ?
-                " and unicode range " + font.unicode_range : ""
-        ));
+        logger.debug(
+            `adding font-face rule for font-family ${font.family.name} with weight ${font.weight} and 
+            style ${font.style}${font.unicode_range ? " and unicode range " + font.unicode_range : ""}`
+        );
 
         ruleset.push(rule);
     };
@@ -313,7 +317,7 @@ FontFaceRenderer = function(endpoint) {
     this._build_ast = function(collection) {
         logger.info("building ast");
 
-        var ast = {
+        const ast = {
             type: "stylesheet",
             stylesheet: {
                 rules: []
@@ -329,40 +333,40 @@ FontFaceRenderer = function(endpoint) {
 FontFaceRenderer.prototype = {
     render: function(collection) {
         logger.info("rendering font-faces");
-        var ast = this._build_ast(collection);
+        const ast = this._build_ast(collection);
         return css.stringify(ast);
     }
 };
 
 module.exports = {
     dump: function(config) {
-        if(!config.hasOwnProperty("url")) {
-            throw new Error("url is mandatory");
-        }
-
-        if(!config.hasOwnProperty("target_directory")) {
-            throw new Error("target_directory is mandatory");
-        }
-
-        if(!config.hasOwnProperty("css_file")) {
-            config.css_file = "fonts.css";
-        }
-
-        var loader = new FontLoader(config.url, config.target_directory);
-        var renderer = new FontFaceRenderer(config.web_directory || "");
-
         return new Promise(function(resolve, reject) {
+            if(!_.has(config, "url")) {
+                throw new Error("url is mandatory");
+            }
+
+            if(!_.has(config, "target_directory")) {
+                throw new Error("target_directory is mandatory");
+            }
+
+            if(!_.has(config, "css_file")) {
+                config.css_file = "fonts.css";
+            }
+
+            const loader = new FontLoader(config.url, config.target_directory);
+            const renderer = new FontFaceRenderer(config.web_directory || "");
+
             loader.request_all().then(
                 function(collection) {
-                    var css = renderer.render(collection);
+                    const css = renderer.render(collection);
                     fs.writeFileSync(path.join(config.target_directory, config.css_file), css);
                     resolve({
                         css: css,
                         fonts: collection
                     });
                 },
-                function() {
-                    logger.error("could not download fonts");
+                function(err) {
+                    logger.error("could not download fonts", err);
                     reject();
                 }
             );
